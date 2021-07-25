d0ce28966e18ebbb84fa9f38423b474935336ef4
Rokon003 <bhuianrokon003@gmail.com>
2021-07-25 14:30:55 +0600

    lab practise

---
+++ MVC/Controllers/productcontroller.php
@@ -0,0 +1,103 @@
+<?php
+    require_once 'Models/db_config.php';
+    $name="";
+	$err_name="";
+    $c_id="";
+    $err_c_id="";
+    $price=""
+    $err_price="";
+    $qty="";
+	$err_qty="";
+    $desc="";
+    $err_desc="";
+    $img=""
+    $err_img="";
+	$err_db="";
+
+    $hasError=false;
+	
+
+	if(isset($_POST["add_products"])){
+        if(empty($_POST["name"])){
+			$err_name="Name Required";
+			$hasError = true;
+		}
+		else{
+			$name=$_POST["name"];
+        }
+        if(empty($_POST["c_id"])){
+			$err_c_id="Valid Id Required";
+			$hasError = true;
+		}
+		else{
+			$c_id=$_POST["c_id"];
+        }
+		if(empty($_POST["price"])){
+			$err_price="Insert Price";
+			$hasError = true;
+		}
+		else{
+			$price=$_POST["price"];
+        }
+		if(empty($_POST["q"])){
+			$err_price="Insert Price";
+			$hasError = true;
+		}
+		else{
+			$price=$_POST["price"];
+        }
+		if(empty($_POST["quantity"])){
+			$err_qty="Insert product quantity";
+			$hasError = true;
+		}
+		else{
+			$qty=$_POST["quantity"];
+        }
+		if(empty($_POST["description"])){
+			$err_desc="Insert description";
+			$hasError = true;
+		}
+		else{
+			$desc=$_POST["description"];
+        }
+		
+		
+        if(!$hasError){
+			
+			$rs=insertProduct($_POST["name"],1,$_POST["price"],$_POST["quantity"],$_POST["description"],"dummy");
+			if($rs===true){
+				header("Location: all_products.php");
+			}
+			$err_db= $rs;
+			
+			
+
+			
+		}
+
+    }
+
+    function insertProduct($name,$c_id,$price,$qty,$desc,$img){
+		$query= "insert into products values (NULL,'$name',$c_id,$price,$qty,'$desc','$img')";
+		return execute($query);	
+	}
+    function getAllProducts(){
+		$query= "select p.*,c.name from products p left join categories c on c.id = p.c_id ";
+        $rs = get($query);
+		return $rs;	
+	}
+
+    function getProducts($id){
+		$query= "select * from products where id=$id";
+        $rs = get($query);
+		return $rs[0];	
+	}
+	function updateProducts($name,$c_id,$price,$qty,$desc,$img,$id){
+		$query= "update products set name='$name', c_id=$c_id, price=$price, quantity=$qty, description='$desc' where id=$id";
+		return execute($query);
+	}
+	function deleteProducts($id){
+		$query= "delete from products where id=$id";
+		return execute($query);
+	}
+?>
---
+++ MVC/add_product.php
@@ -0,0 +1,58 @@
+<?php
+    include 'Controllers/productcontroller.php';
+	$products= getAllProducts();
+<html>
+	<head></head>
+	<body>
+        
+		<fieldset>
+		    <legend align="center"><h1> Add Product:</h1></legend>
+            <h5><?php echo $err_db; ?></h5>
+			<form action="" method="post">
+				<table align="center" >
+					<tr>
+					    <td><b><i>Name: <i><b></td>
+						<td><input type="text" name="name" value="<?php echo $name; ?>"></td>
+						
+					</tr>
+                    <tr>
+						<td><b><i>Category:  <b><i></td>
+						<td>
+							<select name="">
+								<option selected disabled>--Choose--</option>
+							</select> 
+						</td>
+						<td><span><small><?php echo $err_types;?></small></span></td>
+					</tr>
+                    <tr>
+					    <td><b><i>Price: <i><b></td>
+						<td><input type="text" name="price" value="<?php echo $price; ?>"></td>
+						
+					</tr>
+                    <tr>
+					    <td><b><i>Quantity: <i><b></td>
+						<td><input type="text" name="quantity" value="<?php echo $qty; ?>"></td>
+						
+					</tr>
+                    <tr>
+					    <td><b><i>Description: <i><b></td>
+						<td><input type="text" name="description" value="<?php echo $desc; ?>"></td>
+						
+					</tr>
+                    <tr>
+					    <td><b><i>Image: <i><b></td>
+						<td><input type="file" value="<?php echo $img; ?>"></td>
+						
+					</tr>
+                    
+					
+					<tr>
+					    <br><td align="center" colspan="3"><br><br><input type="submit" name="add_product" value="Add Product"></td>
+					</tr>
+					
+				</table>
+			</form>
+		</fieldset>
+		
+	</body>
+</html>
